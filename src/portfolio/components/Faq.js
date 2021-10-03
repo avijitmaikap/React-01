6fd7341842fcd0c03b2578d76f3c3a0fee1fac60
Avijit <maikapavijit@gmail.com>
2021-10-03 08:11:47 -0400

    faq title modified

--- src/portfolio/components/Faq.js
+++ src/portfolio/components/Faq.js
@@ -18,7 +18,7 @@ export default function Faq() {
           <div class="accordion-item">
             <FaqItem
               id="flush-collapseTwo"
-              title="Accordion Item #1"
+              title="Test Title"
               description="Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application."
             />
           </div>
